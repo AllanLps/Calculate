9d550d8cd921cbe22d936b24ac96fb68dfb01ca1
Allan Lopes <allanlps20@gmail.com>
2023-01-13 12:10:08 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Calculator from './components/Calculator';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Calculator IOS made with React.JS</h1>
+      <Calculator></Calculator>
+    </div>
+  );
+}
+
+export default App;
